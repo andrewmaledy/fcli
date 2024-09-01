b42e13f5d9dd7637bc93dcfd6c83a015aa95afa3
Andrew Maledy <andrew.maledy@gmail.com>
2024-09-01 16:30:01 -0700

    Making config file read from home directory (~.fcli-config.yaml)

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,13 +2,18 @@ package config
 
 import (
 	"log"
+	"os/user"
 
 	"github.com/spf13/viper"
 )
 
 func InitConfig() {
 	viper.SetConfigName(".fcli-config")
-	viper.AddConfigPath(".")
+	usr, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+	viper.AddConfigPath(usr.HomeDir)
 	viper.AutomaticEnv()
 
 	if err := viper.ReadInConfig(); err != nil {
